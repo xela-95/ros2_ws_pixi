6f3b81e04caf00c9932582fbe253ad7307e93a7b
Alessandro Croci <alessandro.croci@iit.it>
2025-01-06 18:02:45 +0100

    Add service client to turtle_controller

--- src/my_package/my_package/turtle_controller.py
+++ src/my_package/my_package/turtle_controller.py
@@ -1,13 +1,17 @@
+from functools import partial
+
 import rclpy
 from geometry_msgs.msg import Twist
 from rclpy.node import Node
 from turtlesim.msg import Pose
+from turtlesim.srv import SetPen
 
 
 class TurtleControllerNode(Node):
 
     def __init__(self):
         super().__init__("turtle_controller")
+        self._prev_x = 0.0
         self._pose_subscriber = self.create_subscription(
             Pose, "/turtle1/pose", self.pose_callback, 10
         )
@@ -23,6 +27,35 @@ class TurtleControllerNode(Node):
             cmd.linear.x = 5.0
             cmd.angular.z = 0.0
         self._cmd_vel_pub.publish(cmd)
+
+        if pose.x > 5.5 and self._prev_x <= 5.5:
+            self._prev_x = pose.x
+            self.get_logger().info("Set pen to red")
+            self.call_set_pen_service(255, 0, 0, 3, 0)
+        elif pose.x <= 5.5 and self._prev_x > 5.5:
+            self._prev_x = pose.x
+            self.get_logger().info("Set pen to green")
+            self.call_set_pen_service(0, 255, 0, 3, 0)
+
+    def call_set_pen_service(self, r, g, b, width, off):
+        client = self.create_client(SetPen, "turtle1/set_pen")
+        while not client.service_is_ready():
+            self.get_logger().warn("Waiting for service...")
+        request = SetPen.Request()
+        request.r = r
+        request.g = g
+        request.b = b
+        request.width = width
+        request.off = off
+
+        future = client.call_async(request)
+        future.add_done_callback(partial(self.callback_set_pen))
+
+    def callback_set_pen(self, future):
+        try:
+            response = future.result()
+        except Exception as e:
+            self.get_logger().error(f"Service call failed: {str(e)}")
 
 
 def main(args=None):
