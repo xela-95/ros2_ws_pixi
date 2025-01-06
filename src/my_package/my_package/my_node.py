e6b4077552969ee6208fd6cd37b11cb93e075c09
Alessandro Croci <alessandro.croci@iit.it>
2025-01-06 15:18:14 +0100

    Add timer callback to MyNode for periodic logging

--- src/my_package/my_package/my_node.py
+++ src/my_package/my_package/my_node.py
@@ -5,16 +5,20 @@ from rclpy.node import Node
 class MyNode(Node):
     def __init__(self):
         super().__init__("my_node")
-        self.get_logger().info("ROS2")
+        self._counter = 0
+        self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info("Hello " + str(self._counter))
+        self._counter += 1
 
 
 def main(args=None):
     # Initialize ROS communication
     rclpy.init(args=args)
-
     # Create a node
     node = MyNode()
-
+    # Spin
     rclpy.spin(node)
     # Shutdown ROS communication
     rclpy.shutdown()
