da0916f81ca968058e752d94392438b3b515aa42
Alessandro Croci <alessandro.croci@iit.it>
2025-01-06 14:52:42 +0100

    Stop tracking `install/` `log/` and `build/` dirs

--- build/my_package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/home/acroci/repos/ros2_ws_pixi/.pixi/envs/default':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/acroci/repos/ros2_ws_pixi/install/my_package'
