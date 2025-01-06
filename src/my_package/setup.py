2a04bb7a62c51e8572b34320124687642a537ae1
Alessandro Croci <alessandro.croci@iit.it>
2025-01-06 15:44:48 +0100

    Update setup.py to include draw_circle console script and format code

--- src/my_package/setup.py
+++ src/my_package/setup.py
@@ -4,23 +4,23 @@ package_name = 'my_package'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='acroci',
-    maintainer_email='alessandro.croci@iit.it',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="acroci",
+    maintainer_email="alessandro.croci@iit.it",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'my_node = my_package.my_node:main'
+        "console_scripts": [
+            "my_node = my_package.my_node:main",
+            "draw_circle = my_package.draw_circle:main",
         ],
     },
 )
